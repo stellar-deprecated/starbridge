de7965675a723dbc72422e84a8effadc1cad8543
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-01-24 17:21:00 -0800

    Publish to destination (#25)

--- cmd/gravitybeam/accounts.go
+++ cmd/gravitybeam/accounts.go
@@ -6,17 +6,29 @@ import (
 
 func Accounts(tx *txnbuild.Transaction) []string {
 	accountsMap := map[string]bool{}
+
 	accountsMap[tx.SourceAccount().AccountID] = true
+
 	for _, op := range tx.Operations() {
 		opSource := op.GetSourceAccount()
 		if opSource == "" {
 			continue
 		}
 		accountsMap[opSource] = true
+
+		switch o := op.(type) {
+		case *txnbuild.CreateClaimableBalance:
+			for _, claimant := range o.Destinations {
+				// TODO: Assess the predicate portion of the claimant?
+				accountsMap[claimant.Destination] = true
+			}
+		}
 	}
+
 	accounts := make([]string, 0, len(accountsMap))
 	for a := range accountsMap {
 		accounts = append(accounts, a)
 	}
+
 	return accounts
 }
