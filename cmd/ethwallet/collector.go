54c8358038d47e48a14593c510b769f770584d02
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-02-02 21:26:53 -0800

    fix

--- cmd/ethwallet/collector.go
+++ cmd/ethwallet/collector.go
@@ -5,9 +5,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/sha256"
 	"encoding/hex"
-	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
