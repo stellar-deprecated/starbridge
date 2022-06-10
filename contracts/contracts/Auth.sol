65f1bca79fa7a7d6b0ad4a2d1e5c5014e7d35ebb
tamirms <tamirms@gmail.com>
2022-06-10 20:35:13 +0100

    Add comments

--- contracts/contracts/Auth.sol
+++ contracts/contracts/Auth.sol
@@ -5,13 +5,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Auth {
+    // The validator set is configured by the following three fields:
+    // signers - the full list of signers who can approve a bridge transaction.
+    // Each of the signers has equal wieght.
+    // minThreshold - the minimum amount of signers who need to approve a bridge transaction
+    // for it to be valid.
+    // version - a sequence number associated with the validator set. Whenver the validator
+    // set configuration is updated the version will increment. The current version is part of the
+    // payload for each bridge transaction. So, whenever the version is bumped that will invalidate
+    // any previously signed bridge transactions.
     address[] public signers;
     uint8 public minThreshold;
-     // every time the validator set is updated, the version is incremented.
-     // increasing version numbers ensure that updateSigners() signatures cannot be reused
     uint256 public version;
+    // RegisterSigners is emitted whenever the validator set configuration is modified.
     event RegisterSigners(uint256 version, address[] signers, uint8 minThreshold);
-
+    // fulfilledrequests is a set of all bridge requests which have been completed. This
+    // set is used to prevent an attacker from replaying bridge transactions.
     mapping(bytes32 => bool) private fulfilledrequests;
 
 
@@ -35,6 +44,12 @@ contract Auth {
         emit RegisterSigners(newVersion, _signers, _minThreshold);
     }
 
+    // updateSigners() is called to update the validator set configuration for the bridge.
+    // updateSigners() will bump the version field as a side effect and will emit a RegisterSigners
+    // event detailing the new configuration.
+    // The updateSigners() transactions must be authorized by the previous validator set.
+    // The transactions cannot be replayed because the version field is incremented in updateSigners()
+    // which invalidates all transaction which sign a payload containing older versions.
     function updateSigners(
         address[] calldata _signers, 
         uint8 _minThreshold,
@@ -47,6 +62,8 @@ contract Auth {
         _updateSigners(newVersion, _signers, _minThreshold);
     }
 
+    // verifySignatures() ensure that provided list of signatures map to the validator set
+    // configured for the bridge and that there are at least minThreshold signers.
     function verifySignatures(bytes32 h, bytes[] memory signatures, uint8[] memory indexes)
         internal view
     {
@@ -70,6 +87,11 @@ contract Auth {
         }
     }
 
+    // verifyRequest() will ensure the following three invariants hold
+    // 1) the request is authorized by the bridge validators and a sufficient
+    // number of signatures from the bridge validators.
+    // 2) the request has not been executed before (replay protection).
+    // 3) the request is not expired
     function verifyRequest(
         bytes32 requestHash,
         bytes32 requestID,
@@ -83,6 +105,17 @@ contract Auth {
         require(block.timestamp < expiration, "request is expired");
     }
 
+    // requestStatus() takes a request id and returns whether it was fulfilled
+    // along with the current block.
+    // This function will be invoked by the validators using https://eth.wiki/json-rpc/API#eth_call
+    // in order to check whether a bridge withdrawal / refund was executed.
+    // Returning the current block is also necessary so that the validator
+    // can associate the requestStatus() response with a moment in time.
+    // For example, consider a Stellar -> ETH bridge deposit which occurred at time t.
+    // Assume the validator calls requestStatus() on the transfer id and the response is (false, n).
+    // If the timestamp of block n is greater than t + 24h then we can infer that the transfer
+    // was not claimed on the ETH side of the bridge and that it is safe to authorize a refund on
+    // the Stellar side.
     function requestStatus(bytes32 requestID) external view returns (bool, uint256) {
         return (fulfilledrequests[requestID], block.number);
     }
--- contracts/contracts/Bridge.sol
+++ contracts/contracts/Bridge.sol
@@ -6,81 +6,123 @@ import "./StellarAsset.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+// Every bridge transfer has a globally unique id.
+//
+// For a ETH -> Stellar transfer the id would be keccak256(abi.encode(txHash, logIndex))
+// where txHash is the hash of the ethereum transaction containing the deposit event and
+// logIndex is the index of the deposit event in the ethereum block.
+// The logIndex is necessary because a single ethereum transaction could call
+// depositERC20() / depositETH() multiple times.
+// The bridge smart contract will ensure that a ETH -> Stellar transfer can only be refunded
+// once by maintaining a set of fulfilled ids.
+//
+// For a Stellar -> ETH transfer the id wouold be Stellar transaction hash assuming a
+// stellar transaction can only contain one Stellar -> ETH transfer.
+// Similar to the refund case, the bridge smart contract will ensure that a Stellar -> ETH
+// transfer can only be completed once by mantaining a set of fulfilled ids.
+
+// WithdrawERC20Request is the payload for the withdrawERC20() transaction.
 struct WithdrawERC20Request {
-    bytes32 id;
-    uint256 expiration;
-    address recipient;
-    address token;
-    uint256 amount;
+    bytes32 id; // the id of the transfer
+    uint256 expiration; // unix timestamp of when the transaction should expire
+    address recipient; // ethereum address who will receive the ERC20 tokens
+    address token; // the ERC20 token
+    uint256 amount; // the amount of tokens to be transferred
 }
 
+// WithdrawETHRequest is the payload for the withdrawETH() transaction.
 struct WithdrawETHRequest {
-    bytes32 id;
-    uint256 expiration;
-    address recipient;
-    uint256 amount;
+    bytes32 id; // the id of the transfer
+    uint256 expiration; // unix timestamp of when the transaction should expire
+    address recipient; // ethereum address who will receive the ETH
+    uint256 amount; // the amount of ETH to be transferred
 }
 
-struct CreateStellarAssetRequest {
+// SetPausedRequest is the payload for the setPaused() transaction.
+struct SetPausedRequest {
+    uint8 value; // bitmask representing whether deposits / withdrawals are enabled
+    uint256 nonce; // used to make each transaction unique for replay prevention
+    uint256 expiration; // unix timestamp of when the transaction should expire
+}
+
+// RegisterStellarAssetRequest is the payload for the registerStellarAsset() transaction.
+// The three fields define a new ERC20 token which represents the ethereum version of
+// a Stellar asset.
+struct RegisterStellarAssetRequest {
     uint8 decimals;
     string name;
     string symbol;
 }
 
+// bitwise flag representing the state where no deposits are allowed on the bridge
 uint8 constant PAUSE_DEPOSITS = 1 << 0;
+// bitwise flag representing the state where no withdrawals are allowed on the bridge
 uint8 constant PAUSE_WITHDRAWALS =  1 << 1;
+// bitwise flag representing the state where no withdrawals or deposits are allowed on the bridge
 uint8 constant PAUSE_DEPOSITS_AND_WITHDRAWALS = PAUSE_DEPOSITS | PAUSE_WITHDRAWALS;
 
-contract Bridge is Auth {
-    uint8 public paused;
+// SET_PAUSED_ID is used to distinguish setPaused() signatures from signatures for other bridge functions.
+bytes32 constant SET_PAUSED_ID = keccak256("setPaused");
+// REGISTER_STELLAR_ASSET_ID is used to distinguish registerStellarAsset() signatures from signatures for other bridge functions.
+bytes32 constant REGISTER_STELLAR_ASSET_ID = keccak256("registerStellarAsset");
+// WITHDRAW_ETH_ID is used to distinguish withdrawETH() signatures from signatures for other bridge functions.
+bytes32 constant WITHDRAW_ETH_ID = keccak256("withdrawETH()");
+// WITHDRAW_ERC20_ID is used to distinguish withdrawERC20() signatures from signatures for other bridge functions.
+bytes32 constant WITHDRAW_ERC20_ID = keccak256("withdrawERC20()");
 
+contract Bridge is Auth {
+    // paused is a bitmask which determines whether deposits / withdrawals are enabled on the bridge
+    uint8 public paused;
+    // SetPaused is emitted whenever the paused state of the bridge changes
     event SetPaused(uint8 value);
 
+    // to create a Bridge instance you need to provide the validator set configuration
     constructor(address[] memory _signers, uint8 _minThreshold) Auth(_signers, _minThreshold) {}
 
+    // DepositERC20 is emitted whenever an ERC20 token is deposited on the bridge.
+    // DepositERC20 initiates a ERC20 -> Stellar transfer.
     event DepositERC20(
         address token,
         address sender,
         uint256 destination,
         uint256 amount
     );
+    // DepositERC20 is emitted whenever ETH is deposited on the bridge.
+    // DepositETH initiates a ETH -> Stellar transfer.
     event DepositETH(
         address sender,
         uint256 destination,
         uint256 amount
     );
 
-    // Every bridge transfer has a globally unique id.
-    //
-    // For a ETH -> Stellar transfer the id would be keccak256(abi.encode(txHash, logIndex))
-    // where txHash is the hash of the ethereum transaction containing the deposit event and
-    // logIndex is the index of the deposit event in the ethereum block.
-    // The logIndex is necessary because a single ethereum transaction could call
-    // depositERC20() / depositETH() multiple times.
-    // The bridge smart contract will ensure that a ETH -> Stellar transfer can only be refunded
-    // once by maintaining a set of fulfilled ids.
-    //
-    // For a Stellar -> ETH transfer the id wouold be Stellar transaction hash assuming a
-    // stellar transaction can only contain one Stellar -> ETH transfer.
-    // Similar to the refund case, the bridge smart contract will ensure that a Stellar -> ETH
-    // transfer can only be completed once by mantaining a set of fulfilled ids.
-
+    // WithdrawERC20 is emitted whenever an ERC20 token is claimed from the bridge.
+    // WithdrawERC20 corresponds to completing a Stellar -> ERC20 transfer or
+    // refunding a ERC20 -> Stellar transfer.
     event WithdrawERC20(
         bytes32 id,
         address recipient,
         address token,
         uint256 amount
     );
+    // WithdrawERC20 is emitted whenever ETH is claimed from the bridge.
+    // WithdrawERC20 corresponds to completing a Stellar -> ETH transfer or
+    // refunding a ETH -> Stellar transfer.
     event WithdrawETH(
         bytes32 id,
         address recipient,
         uint256 amount
     );
 
+    // RegisterStellarAsset is emitted whenever an ERC20 token is created
+    // to represent a Stellar asset.
     event RegisterStellarAsset(address asset);
 
+    // isStellarAsset identifies whether an ERC20 token is a Stellar asset
+    // created by the bridge.
     mapping(address => bool) public isStellarAsset;
 
+    // depositERC20() deposits ERC20 tokens to the bridge and starts a ERC20 -> Stellar
+    // transfer. If deposits are disabled this function will fail.
     function depositERC20(
         address token,
         uint256 destination,
@@ -101,12 +143,20 @@ contract Bridge is Auth {
         emit DepositERC20(token, msg.sender, destination, amount);
     }
 
+    // depositETH() deposits ETH to the bridge and starts a ETH -> Stellar
+    // transfer. If deposits are disabled this function will fail.
     function depositETH(uint256 destination) external payable {
         require((paused & PAUSE_DEPOSITS) == 0, "deposits are paused");
         require(msg.value > 0);
         emit DepositETH(msg.sender, destination, msg.value);
     }
 
+    // withdrawERC20() claims ERC20 tokens from the bridge. This can correspond to
+    // fulfilling a Stellar -> ERC20 transfer or refunding a ERC20 -> Stellar transfer.
+    // If withdrawals are disabled this function will fail.
+    // withdrawERC20() must be authorized by the bridge validators otherwise the transaction
+    // will fail. Replay prevention is implemented using the transfer id.
+    // Any attempts to withdraw multiple times for the same transfer will fail.
     function withdrawERC20(
         WithdrawERC20Request calldata request,
         bytes[] calldata signatures,
@@ -114,7 +164,7 @@ contract Bridge is Auth {
     ) external {
         require((paused & PAUSE_WITHDRAWALS) == 0, "withdrawals are paused");
         verifyRequest(
-            keccak256(abi.encode(version, request)),
+            keccak256(abi.encode(version, WITHDRAW_ERC20_ID, request)),
             request.id,
             request.expiration,
             signatures,
@@ -138,6 +188,12 @@ contract Bridge is Auth {
         );
     }
 
+    // withdrawETH() claims ETH tokens from the bridge. This can correspond to
+    // fulfilling a Stellar -> ETH transfer or refunding a ETH -> Stellar transfer.
+    // If withdrawals are disabled this function will fail.
+    // withdrawETH() must be authorized by the bridge validators otherwise the transaction
+    // will fail. Replay prevention is implemented using the transfer id.
+    // Any attempts to withdraw multiple times for the same transfer will fail.
     function withdrawETH(
         WithdrawETHRequest calldata request,
         bytes[] calldata signatures,
@@ -145,7 +201,7 @@ contract Bridge is Auth {
     ) external {
         require((paused & PAUSE_WITHDRAWALS) == 0, "withdrawals are paused");
         verifyRequest(
-            keccak256(abi.encode(version, request)),
+            keccak256(abi.encode(version, WITHDRAW_ETH_ID, request)),
             request.id, 
             request.expiration, 
             signatures,
@@ -156,28 +212,35 @@ contract Bridge is Auth {
         emit WithdrawETH(request.id, request.recipient, request.amount);
     }
 
+    // setPaused() will enable or disable withdrawals / deposits.
+    // setPaused() must be authorized by the bridge validators otherwise the transaction
+    // will fail. Replay prevention is implemented by storing the request hash in the
+    // fulfilledrequests set.
     function setPaused(
-        uint8 value,
-        uint256 nonce, // used to make each request unique
-        uint256 expiration, // allow requests to be short lived
+        SetPausedRequest memory request,
         bytes[] calldata signatures,
         uint8[] calldata indexes
     ) external {
-        require(value <= PAUSE_DEPOSITS_AND_WITHDRAWALS, "invalid paused value");
-        bytes32 requestHash = keccak256(abi.encode(version, keccak256("setPaused"), value, nonce, expiration));
+        require(request.value <= PAUSE_DEPOSITS_AND_WITHDRAWALS, "invalid paused value");
+        bytes32 requestHash = keccak256(abi.encode(version, SET_PAUSED_ID, request));
         // ensure the same setPaused() transaction cannot be used more than once
-        verifyRequest(requestHash, requestHash, expiration, signatures, indexes);
-        paused = value;
-        emit SetPaused(value);
+        verifyRequest(requestHash, requestHash, request.expiration, signatures, indexes);
+        paused = request.value;
+        emit SetPaused(request.value);
     }
 
+    // registerStellarAsset() will creates an ERC20 token to represent a stellar asset.
+    // registerStellarAsset() must be authorized by the bridge validators otherwise the transaction
+    // will fail. Replay prevention is impemented by creating the ERC20 via the CREATE2 opcode (see
+    // https://eips.ethereum.org/EIPS/eip-1014 )
     function registerStellarAsset(
-        CreateStellarAssetRequest memory request,
+        RegisterStellarAssetRequest memory request,
         bytes[] calldata signatures,
         uint8[] calldata indexes
     ) external {
         bytes32 requestHash = keccak256(abi.encode(
             version,
+            REGISTER_STELLAR_ASSET_ID,
             request.decimals,
             keccak256(bytes(request.name)),
             keccak256(bytes(request.symbol))
