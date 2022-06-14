49b06b3eadad578ea272fe94628b6726017bd64b
tamirms <tamirms@gmail.com>
2022-06-14 22:35:38 +0100

    Add deploy script for testnet

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -11,7 +11,17 @@ module.exports = {
       optimizer: {
         enabled: true,
         runs: 20000
-      }
+      },
     }
   },
+  networks: {
+    goerli: {
+      url: "https://ethereum-goerli-rpc.allthatnode.com/",
+      accounts: [
+        "51138e68e8a5fa906d38c5b42bc01b805d7adb3fce037743fb406bb10aa83307",
+        "cff41ce3c1708e589b87198c9ee494eef407ca2a765a4353cf162c85ddc81cd9",
+        "0b1037a08795be0955e39e7e279e0530eb89e0ec06d372ff6f122a5a4e1a6f84",
+      ]
+    }
+  }
 };
--- contracts/test/Bridge.js
+++ contracts/test/Bridge.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Bridge", function() {
+describe("Deploy Bridge", function() {
     it("deploy Bridge contract with 20 signers", async function() {
       const signers = (await ethers.getSigners()).slice(0, 20);
       signers.sort((a, b) => a.address.toLowerCase().localeCompare(b.address.toLowerCase()));
@@ -20,4 +20,42 @@ describe("Bridge", function() {
       const gas = await bridge.estimateGas.updateSigners(addresses, 20, signatures, [...Array(20).keys()]);
       console.log(gas);
     });
+
+    it("deploy Bridge contract with invalid minThreshold", async function() {
+      const signers = (await ethers.getSigners()).slice(0, 20);
+      signers.sort((a, b) => a.address.toLowerCase().localeCompare(b.address.toLowerCase()));
+      const Bridge = await ethers.getContractFactory("Bridge");
+      const addresses = signers.map(a => a.address);
+
+      for (let i = 0; i <= 10; i++) {
+        await expect(Bridge.deploy(addresses, i)).to.be.revertedWith("min threshold is too low");
+      }
+
+      for (let i = 11; i <= 20; i++) {
+        const bridge = await Bridge.deploy(addresses, i);
+        expect(await bridge.minThreshold()).to.equal(i);
+        expect(await bridge.version()).to.equal(0);
+      }
+
+      await expect(Bridge.deploy(addresses, 21)).to.be.revertedWith("min threshold is too high");
+    });
+
+    it("deploy Bridge contract with invalid signers", async function() {
+      const signers = [];
+      for (let i = 0; i < 256; i++) {
+        signers.push(ethers.Wallet.createRandom())
+      }
+      signers.sort((a, b) => a.address.toLowerCase().localeCompare(b.address.toLowerCase()));
+
+
+      const Bridge = await ethers.getContractFactory("Bridge");
+      const addresses = signers.map(a => a.address);
+
+      await expect(Bridge.deploy([], 0)).to.be.revertedWith("too few signers");
+      await expect(Bridge.deploy(addresses, 255)).to.be.revertedWith("too many signers");
+      await expect(Bridge.deploy(addresses.slice(0, 255), 255)).to.not.be.reverted;
+      await expect(Bridge.deploy([addresses[0], addresses[0], addresses[1]], 3)).to.be.revertedWith("signers not sorted");
+      await expect(Bridge.deploy([addresses[0], addresses[1], addresses[2], addresses[4], addresses[3]], 5)).to.be.revertedWith("signers not sorted");
+    });
+
   });
