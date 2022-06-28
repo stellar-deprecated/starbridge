b24db5ed79cabb9c8930c94552ffdb5cda818d48
Giuliano Losa <giuliano@losa.fr>
2022-06-28 10:54:05 -0700

    expose num threads of poisonivy

--- docs/docker/plot_dependencies.sh
+++ docs/docker/plot_dependencies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 FILE=$1
+NUM_PROC=$2
 NAME=${FILE%.*}
 cd /home/user/output/
-# analyze file using 2 threads:
-python /home/user/poison-ivy/poisonivy.py /home/user/models/$FILE 2
+# analyze file using NUM_PROC threads:
+python /home/user/poison-ivy/poisonivy.py /home/user/models/$FILE $NUM_PROC
 # make png file:
 dot -Tpng -o $NAME-invariants.png $NAME.dot
