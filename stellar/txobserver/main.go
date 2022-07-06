44c2d8e13456285715d01899e8aec1bdc3c9aa8a
Bartek Nowotarski <bartek@nowotarski.info>
2022-07-06 13:09:10 +0200

    Add catchup mode to Stellar Observer (#70)
    When a new validator starts (or existing one lost it's DB) it needs to process
    all withdrawal transactions to prevent double spend. This commit adds this mode
    that will run on the first call to `ProcessNewLedgers`.

--- stellar/txobserver/main.go
+++ stellar/txobserver/main.go
@@ -10,7 +10,9 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/protocols/horizon"
+	"github.com/stellar/go/protocols/horizon/operations"
 	slog "github.com/stellar/go/support/log"
+	"github.com/stellar/go/toid"
 	"github.com/stellar/starbridge/store"
 )
 
@@ -23,23 +25,34 @@ type Observer struct {
 	store  *store.DB
 	log    *slog.Entry
 
-	bridgeAccountCreateSequence uint32
+	ledgerSequence uint32
+	catchup        bool
 }
 
 func NewObserver(
 	ctx context.Context,
 	bridgeAccount string,
-	bridgeAccountCreateSequence uint32,
 	client *horizonclient.Client,
 	store *store.DB,
 ) *Observer {
 	o := &Observer{
-		ctx:                         ctx,
-		bridgeAccount:               bridgeAccount,
-		bridgeAccountCreateSequence: bridgeAccountCreateSequence,
-		client:                      client,
-		store:                       store,
-		log:                         slog.DefaultLogger.WithField("service", "stellar_txobserver"),
+		ctx:           ctx,
+		bridgeAccount: bridgeAccount,
+		client:        client,
+		store:         store,
+		log:           slog.DefaultLogger.WithField("service", "stellar_txobserver"),
+	}
+
+	ledgerSeq, err := o.store.GetLastLedgerSequence(context.Background())
+	if err != nil {
+		o.log.Fatalf("Unable to load last ledger sequence from db: %v", err)
+	}
+
+	if ledgerSeq == 0 {
+		// Perform catchup on the first call to ProcessNewLedgers
+		o.catchup = true
+	} else {
+		o.ledgerSequence = ledgerSeq
 	}
 
 	return o
@@ -47,16 +60,17 @@ func NewObserver(
 
 func (o *Observer) ProcessNewLedgers() {
 	for o.ctx.Err() == nil {
-		if ledgerSeq, err := o.store.GetLastLedgerSequence(context.Background()); err != nil {
-			o.log.Errorf("Unable to load last ledger sequence from db: %v", err)
-		} else {
-			if ledgerSeq == 0 {
-				ledgerSeq = o.bridgeAccountCreateSequence
+		if o.catchup {
+			err := o.catchupLedgers()
+			if err != nil {
+				o.log.WithFields(slog.F{"error": err}).Error("Error catching up")
 			} else {
-				ledgerSeq++
+				o.catchup = false
 			}
+		} else {
 			// Get ledger data first to ensure there are no gaps
-			if ledger, err := o.client.LedgerDetail(ledgerSeq); err != nil {
+			ledger, err := o.client.LedgerDetail(o.ledgerSequence)
+			if err != nil {
 				if herr, ok := err.(*horizonclient.Error); ok && herr.Response.StatusCode == http.StatusNotFound {
 					// Ledger not found means we reached the latest ledger
 					return
@@ -64,16 +78,95 @@ func (o *Observer) ProcessNewLedgers() {
 					o.log.WithField("error", err).Error("Error getting ledger details")
 				}
 			} else {
-				o.log.WithField("sequence", ledgerSeq).Info("Processing ledger...")
-				if err := o.processSingleLedger(ledger); err != nil {
-					o.log.WithFields(slog.F{"error": err, "sequence": ledgerSeq}).Error("Error processing a single ledger details")
+				o.log.WithField("sequence", o.ledgerSequence).Info("Processing ledger...")
+
+				err = o.processSingleLedger(ledger)
+				if err != nil {
+					o.log.WithFields(slog.F{"error": err, "sequence": o.ledgerSequence}).Error("Error processing a single ledger details")
 				} else {
+					o.ledgerSequence++
 					continue // without time.Sleep
 				}
 			}
 		}
 		time.Sleep(time.Second)
 	}
+}
+
+func (o *Observer) catchupLedgers() error {
+	root, err := o.client.Root()
+	if err != nil {
+		o.log.Fatalf("Unable to access Horizon (%s) root resource: %v", o.client.HorizonURL, err)
+	}
+
+	ledgerSeq := root.HorizonSequence
+
+	o.log.Infof("Catching up to ledger %d", ledgerSeq)
+
+	err = o.store.Session.Begin()
+	if err != nil {
+		return errors.Wrap(err, "error starting a transaction")
+	}
+
+	defer func() {
+		_ = o.store.Session.Rollback()
+	}()
+
+	// Process past bridge account payments
+	cursor := toid.AfterLedger(ledgerSeq).String()
+	previousHash := ""
+	var lastOp operations.Operation
+	for o.ctx.Err() == nil {
+		ops, err := o.client.Payments(horizonclient.OperationRequest{
+			ForAccount: o.bridgeAccount,
+			Cursor:     cursor,
+			Order:      horizonclient.OrderDesc,
+			Limit:      200,
+			Join:       "transactions",
+		})
+		if err != nil {
+			return errors.Wrap(err, "error getting operations")
+		}
+
+		if len(ops.Embedded.Records) == 0 {
+			break
+		}
+
+		err = o.processOpsSinglePage(ops.Embedded.Records, previousHash)
+		if err != nil {
+			return err
+		}
+
+		lastOp = ops.Embedded.Records[len(ops.Embedded.Records)-1]
+		cursor = lastOp.PagingToken()
+		previousHash = lastOp.GetBase().TransactionHash
+	}
+
+	if o.ctx.Err() != nil {
+		return o.ctx.Err()
+	}
+
+	// At this point we reached the beginning of account history. Ensure the
+	// first op is creating it.
+	if createAccountOp, ok := lastOp.(operations.CreateAccount); !(ok && createAccountOp.Account == o.bridgeAccount) {
+		o.log.Fatal("Reached the end of history but operation creating bridge account not found")
+	}
+
+	// Update sequence number to the ledgerSeq-1
+	// Ledger close time will be updated after returning to ProcessNewLedgers.
+	err = o.store.UpdateLastLedgerSequence(context.TODO(), uint32(ledgerSeq)-1)
+	if err != nil {
+		return errors.Wrap(err, "error updating last ledger sequence")
+	}
+
+	err = o.store.Session.Commit()
+	if err != nil {
+		return errors.Wrap(err, "error commiting a transaction")
+	}
+
+	o.ledgerSequence = uint32(ledgerSeq)
+
+	return nil
 }
 
 func (o *Observer) processSingleLedger(ledger horizon.Ledger) error {
@@ -104,39 +197,14 @@ func (o *Observer) processSingleLedger(ledger horizon.Ledger) error {
 			break
 		}
 
-		for _, op := range ops.Embedded.Records {
-			baseOp := op.GetBase()
-
-			// Update cursor instantly because we can continue later
-			cursor = op.PagingToken()
-
-			// Ignore ops not coming from bridge account
-			if baseOp.SourceAccount != o.bridgeAccount {
-				continue
-			}
-
-			tx := baseOp.Transaction
-			if tx.MemoType != "hash" || tx.Memo == "" || !tx.Successful ||
-				// Skip inserting transactions with multiple ops. Currently Starbridge
-				// does not create such transactions but it can change in the future.
-				previousHash == baseOp.TransactionHash {
-				continue
-			}
-			memoBytes, err := base64.StdEncoding.DecodeString(tx.Memo)
-			if err != nil {
-				return errors.Wrapf(err, "error decoding memo: %s", tx.Memo)
-			}
-
-			err = o.store.InsertHistoryStellarTransaction(context.TODO(), store.HistoryStellarTransaction{
-				Hash:     tx.Hash,
-				Envelope: tx.EnvelopeXdr,
-				MemoHash: hex.EncodeToString(memoBytes),
-			})
-			if err != nil {
-				return errors.Wrapf(err, "error inserting history transaction: %s", tx.Hash)
-			}
-			previousHash = baseOp.TransactionHash
+		err = o.processOpsSinglePage(ops.Embedded.Records, previousHash)
+		if err != nil {
+			return err
 		}
+
+		lastOp := ops.Embedded.Records[len(ops.Embedded.Records)-1].GetBase()
+		cursor = lastOp.PagingToken()
+		previousHash = lastOp.TransactionHash
 	}
 
 	err = o.store.UpdateLastLedgerSequence(context.TODO(), uint32(ledger.Sequence))
@@ -155,5 +223,41 @@ func (o *Observer) processSingleLedger(ledger horizon.Ledger) error {
 	}
 
 	o.log.WithField("sequence", ledger.Sequence).Info("Processed ledger")
+	return nil
+}
+
+func (o *Observer) processOpsSinglePage(ops []operations.Operation, previousHash string) error {
+	for _, op := range ops {
+		baseOp := op.GetBase()
+
+		// Ignore ops not coming from bridge account
+		if baseOp.SourceAccount != o.bridgeAccount {
+			continue
+		}
+
+		tx := baseOp.Transaction
+		if tx.MemoType != "hash" || tx.Memo == "" || !tx.Successful ||
+			// Skip inserting transactions with multiple ops. Currently Starbridge
+			// does not create such transactions but it can change in the future.
+			previousHash == baseOp.TransactionHash {
+			continue
+		}
+
+		memoBytes, err := base64.StdEncoding.DecodeString(tx.Memo)
+		if err != nil {
+			return errors.Wrapf(err, "error decoding memo: %s", tx.Memo)
+		}
+
+		err = o.store.InsertHistoryStellarTransaction(context.TODO(), store.HistoryStellarTransaction{
+			Hash:     tx.Hash,
+			Envelope: tx.EnvelopeXdr,
+			MemoHash: hex.EncodeToString(memoBytes),
+		})
+		if err != nil {
+			return errors.Wrapf(err, "error inserting history transaction: %s", tx.Hash)
+		}
+		previousHash = baseOp.TransactionHash
+	}
+
 	return nil
 }
