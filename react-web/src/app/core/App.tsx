ab33edca0aa1c7ee4250a112add91063412d430f
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-08-19 15:24:41 -0300

    chore: Remove border colors

--- react-web/src/app/core/App.tsx
+++ react-web/src/app/core/App.tsx
@@ -12,19 +12,6 @@ import { CoreRouter } from 'app/core/routes'
 const App = (): JSX.Element => (
   <>
     <CoreRouter />
-    <Heading level={TypographyHeadingLevel.h1} text="Hsomething" />
-    <Label text="Label" />
-    <Link text="Link" />
-    <Paragraph text="Paragraph" />
-    <Typography variant={TypographyVariant.h1} text="h1" />
-    <Typography variant={TypographyVariant.h2} text="h2" />
-    <Typography variant={TypographyVariant.h3} text="h3" />
-    <Typography variant={TypographyVariant.h4} text="h4" />
-    <Typography variant={TypographyVariant.h5} text="h5" />
-    <Typography variant={TypographyVariant.h6} text="h6" />
-    <Typography variant={TypographyVariant.label} text="Label" />
-    <Typography variant={TypographyVariant.link} text="Link" />
-    <Typography variant={TypographyVariant.p} text="Paragraph" />
   </>
 )
 
