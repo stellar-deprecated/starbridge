f8058cc8d8cde65ddb2ff7f040a744b90e7f0534
welladam <wellington.f.adam@gmail.com>
2022-08-22 14:04:52 -0300

    feat: change button component

--- react-web/src/components/atoms/button/index.tsx
+++ react-web/src/components/atoms/button/index.tsx
@@ -1,4 +1,7 @@
 import { Button as SButton } from '@stellar/design-system'
+import classNames from 'classnames'
+
+import styles from './styles.module.scss'
 
 export enum ButtonVariant {
   primary = 'primary',
@@ -6,24 +9,38 @@ export enum ButtonVariant {
   tertiary = 'tertiary',
 }
 
-export enum ButtonSize {
-  default = 'default',
-  small = 'small',
-}
-
 export interface IButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
   iconLeft?: React.ReactNode
   iconRight?: React.ReactNode
   variant?: ButtonVariant
   isLoading?: boolean
-  size?: ButtonSize
   fullWidth?: boolean
+  disabled?: boolean
   children: string | React.ReactNode
 }
 
 const Button = (props: IButtonProps): JSX.Element => {
-  return <SButton {...props} />
+  const {
+    variant = ButtonVariant.primary,
+    fullWidth,
+    className,
+    ...rest
+  } = props
+  const fullWidthStyle = fullWidth && styles.fullWidth
+
+  return (
+    <SButton
+      className={classNames(
+        styles.button,
+        styles[variant],
+        fullWidthStyle,
+        className
+      )}
+      {...rest}
+    />
+  )
 }
 
 export { Button }
