ebdc5b8e0f2dd9af2f413dc3adef520df3bb420d
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-08-23 11:14:21 -0300

    feat: Create input component

--- react-web/src/app/core/App.tsx
+++ react-web/src/app/core/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
+
+import { InputText } from 'components/atoms/input'
 
 import { CoreRouter } from 'app/core/routes'
 
-const App = (): JSX.Element => <CoreRouter />
+const App = (): JSX.Element => {
+  const { register, handleSubmit, watch } = useForm({})
+
+  return (
+    <InputText
+      {...register('eventInformation')}
+      placeholder="Event information"
+      // className={styles.input}
+    />
+  )
+}
 
 export default App
