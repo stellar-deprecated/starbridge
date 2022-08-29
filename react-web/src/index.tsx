c0e9e603bbe479f7d7792e3622ccb1e8e4c34581
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-08-29 10:10:09 -0300

    feat: Create header

--- react-web/src/index.tsx
+++ react-web/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 
 import '@stellar/design-system/build/styles.min.css'
 
+import { Header } from 'components/organisms/header'
+
 import App from 'app/core/App'
 
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
