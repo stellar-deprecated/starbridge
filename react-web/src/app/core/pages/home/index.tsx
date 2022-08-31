b55097a3987d2b8dcb04f4ca3d2ea53dbb4fcb69
welladam <wellington.f.adam@gmail.com>
2022-08-31 15:04:51 -0300

    feat: create home template

--- react-web/src/app/core/pages/home/index.tsx
+++ react-web/src/app/core/pages/home/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 
+import { HomeTemplate } from 'components/templates/home'
+
 import logo from 'app/core/resources/logo.svg'
 
 import styles from './styles.module.scss'
 
 const Home = (): JSX.Element => {
-  return (
-    <main>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>Welcome to CKL Boilerplate</p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </main>
-  )
+  return <HomeTemplate />
 }
 
 export default Home
