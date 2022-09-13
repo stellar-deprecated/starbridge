3af802edff8df0dc17477625a7bfaecabd32bb6d
IsabellePinheiro <isabellepinheiro00@gmail.com>
2022-09-13 17:16:13 -0300

    feat: Create currency logic

---
+++ react-web/src/components/organisms/wallet-input/index.tsx
@@ -0,0 +1,43 @@
+import {
+  Button,
+  ButtonVariant,
+  ButtonSize,
+  Typography,
+  TypographyVariant,
+} from 'components/atoms'
+import { ICurrencyProps, InputLabel, LabeledInput } from 'components/molecules'
+import { IInputProps } from 'components/molecules/labeled-input/type'
+
+import styles from './styles.module.scss'
+
+interface IWalletInputProps extends IInputProps {
+  currency: ICurrencyProps
+  isSender?: boolean
+}
+
+export const WalletInput = ({
+  isSender,
+  currency,
+  onChange,
+}: IWalletInputProps): JSX.Element => {
+  return (
+    <div className={styles.inputContainer}>
+      <div className={styles.content}>
+        <Typography
+          className={styles.label}
+          variant={TypographyVariant.p}
+          text={isSender ? 'From:' : 'To:'}
+        />
+        <Button variant={ButtonVariant.secondary} size={ButtonSize.small}>
+          {`Connect ${currency.label} Wallet`}
+        </Button>
+      </div>
+      <LabeledInput
+        name={currency.initials}
+        label={isSender ? InputLabel.sending : InputLabel.receive}
+        currency={currency}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
--- react-web/src/components/templates/home/index.tsx
+++ react-web/src/components/templates/home/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useEffect, useRef, useState } from 'react'
+
 import {
   Button,
   ButtonVariant,
@@ -5,62 +7,74 @@ import {
   Typography,
   TypographyVariant,
 } from 'components/atoms'
-import { InputLabel, LabeledInput } from 'components/molecules'
-import { CurrencyLabel } from 'components/types/currency'
+import { ICurrencyProps } from 'components/molecules'
+import { WalletInput } from 'components/organisms/wallet-input'
+import { Currency, CurrencyLabel } from 'components/types/currency'
 
+import Eth from 'app/core/resources/eth.svg'
 import SwitchIcon from 'app/core/resources/switch.svg'
+import Weth from 'app/core/resources/weth.svg'
 
 import styles from './styles.module.scss'
 
 export interface IHomeTemplateProps {
   transactionTitle?: string
+  handleSubmit: (evt: FormEvent<HTMLFormElement>) => Promise<void>
 }
 
-interface IWalletInputProps {
-  currency: CurrencyLabel
-  isSender?: boolean
-}
+const HomeTemplate = ({ handleSubmit }: IHomeTemplateProps): JSX.Element => {
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false)
+  const sendingRef = useRef<HTMLInputElement>(null)
+  const receivingRef = useRef<HTMLInputElement>(null)
+  const [toggleSwitchTitle, setToggleSwitchTitle] = useState(true)
+  const [inputSent, setInputSent] = useState('')
+  const [inputReceived, setInputReceived] = useState('')
+  const [currencyFrom, setCurrencyFrom] = useState(Currency.ETH)
+  const [currencyTo, setCurrencyTo] = useState(Currency.WETH)
 
-const WalletInput = ({
-  isSender,
-  currency,
-}: IWalletInputProps): JSX.Element => {
-  const settings = {
-    name: {
-      [CurrencyLabel.weth]: 'stellar wallet input',
-      [CurrencyLabel.eth]: 'ethereum wallet input',
+  const currencyPropsConverter: Record<Currency, ICurrencyProps> = {
+    [Currency.WETH]: {
+      initials: CurrencyLabel.weth,
+      label: Currency.WETH,
+      iconPath: Weth,
     },
-    label: isSender ? InputLabel.sending : InputLabel.receive,
-    button: {
-      [CurrencyLabel.weth]: 'Connect Stellar Wallet',
-      [CurrencyLabel.eth]: 'Connect Ethereum Wallet',
+    [Currency.ETH]: {
+      initials: CurrencyLabel.eth,
+      label: Currency.ETH,
+      iconPath: Eth,
     },
   }
 
-  return (
-    <div className={styles.inputContainer}>
-      <div className={styles.content}>
-        <Typography
-          className={styles.label}
-          variant={TypographyVariant.p}
-          text={isSender ? 'From:' : 'To:'}
-        />
-        <Button variant={ButtonVariant.secondary} size={ButtonSize.small}>
-          {settings.button[currency]}
-        </Button>
-      </div>
-      <LabeledInput
-        name={settings.name[currency]}
-        label={settings.label}
-        currency={currency}
-      />
-    </div>
-  )
-}
+  const onInputReceiveChange = (
+    evt: React.FormEvent<HTMLInputElement>
+  ): void => {
+    const input = evt.target as HTMLInputElement
+    setInputReceived(input.value)
+  }
 
-const HomeTemplate = ({
-  transactionTitle = 'Ethereum -> Stellar',
-}: IHomeTemplateProps): JSX.Element => {
+  const onInputSentChange = (evt: React.FormEvent<HTMLInputElement>): void => {
+    const input = evt.target as HTMLInputElement
+
+    setInputSent(input.value)
+  }
+
+  useEffect(() => {
+    if (inputSent > '0' && inputReceived > '0') {
+      setIsButtonEnabled(true)
+    } else {
+      setIsButtonEnabled(false)
+    }
+  }, [inputSent, inputReceived])
+
+  const changeCurrencyTitle = (): void => {
+    setCurrencyFrom(prev =>
+      prev === Currency.ETH ? Currency.WETH : Currency.ETH
+    )
+    setCurrencyTo(prev =>
+      prev === Currency.ETH ? Currency.WETH : Currency.ETH
+    )
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
@@ -68,22 +82,46 @@ const HomeTemplate = ({
           <Typography
             className={styles.title}
             variant={TypographyVariant.h3}
-            text={transactionTitle}
+            text={toggleSwitchTitle && `${currencyFrom} -> ${currencyTo}`}
           />
           <Button
             className={styles.button}
             variant={ButtonVariant.primary}
             size={ButtonSize.small}
             iconLeft={<img src={SwitchIcon} alt="Switch Icon" />}
+            onClick={changeCurrencyTitle}
           >
             Switch
           </Button>
         </div>
-        <WalletInput isSender currency={CurrencyLabel.eth} />
-        <WalletInput currency={CurrencyLabel.weth} />
-        <Button variant={ButtonVariant.primary} fullWidth>
-          Send Transfer
-        </Button>
+        <div className={styles.form}>
+          <form data-testid="form" onSubmit={handleSubmit}>
+            <div className={styles.formRow}>
+              <WalletInput
+                isSender
+                currency={currencyPropsConverter[currencyFrom]}
+                onChange={onInputSentChange}
+                name={'sending'}
+                ref={sendingRef}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <WalletInput
+                currency={currencyPropsConverter[currencyTo]}
+                onChange={onInputReceiveChange}
+                name={'receiving'}
+                ref={receivingRef}
+              />
+            </div>
+            <Button
+              variant={ButtonVariant.primary}
+              fullWidth
+              disabled={!isButtonEnabled}
+            >
+              Send Transfer
+            </Button>
+          </form>
+        </div>
       </div>
     </main>
   )
