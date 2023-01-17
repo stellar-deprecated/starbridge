fe92f2157a5c05941c22ee530f3c28c02400781b
tamirms <tamirms@gmail.com>
2023-01-17 19:30:10 +0000

    implement deposit() and use invoker for simplified auth

--- soroban-bridge/src/lib.rs
+++ soroban-bridge/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 use soroban_auth::{Identifier, Signature};
-use soroban_sdk::{contractimpl, symbol, contracttype, AccountId, BytesN, Env};
+use soroban_sdk::{contractimpl, symbol, contracttype, AccountId, BytesN, Env, Vec};
 
 mod token {
     soroban_sdk::contractimport!(file = "./soroban_token_spec.wasm");
@@ -33,17 +33,25 @@ impl Bridge {
         panic!("unimplemented");
     }
 
-    pub fn deposit(env: Env, from: Signature, token: BytesN<32>, eth_destination: AccountId, amount: i128) {
-        let topics = (symbol!("deposit"), from.identifier(&env), eth_destination);
+    pub fn deposit(env: Env, token: BytesN<32>, is_wrapped_asset: bool, eth_destination: AccountId, amount: i128) {
+        let client = token::Client::new(&env, &token);
+        let from = &env.invoker().into();
+
+        if is_wrapped_asset {
+            client.burn_from(&Signature::Invoker, &0, &from, &amount);
+        } else {
+            client.xfer_from(&Signature::Invoker, &0, from, &Identifier::Contract(env.current_contract()), &amount);
+        }
+
+        let topics = (symbol!("deposit"), &token, from, eth_destination);
         env.events().publish(topics, amount);
+    }
+
+    pub fn withdraw(env: Env, token: BytesN<32>, is_wrapped_asset: bool, recipient: Identifier, id: BytesN<32>, expiration: u64) {
         panic!("unimplemented");
     }
 
-    pub fn withdraw(env: Env, admin: Signature, token: BytesN<32>, recipient: Identifier, id: BytesN<32>, expiration: u64) {
-        panic!("unimplemented");
-    }
-
-    pub fn set_paused(env: Env, admin: Signature, state : Pause) {
+    pub fn set_paused(env: Env, state : Pause) {
         panic!("unimplemented");
     }
 
